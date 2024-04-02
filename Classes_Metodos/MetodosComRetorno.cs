484728ad4e18d907fe8216ff64f3a2274b8b953e
isaias.costa <isaias.costa@ATA-TI20>
2024-04-02 11:24:17 -0300

    Commit metodo  com retormo

--- Classes_Metodos/MetodosComRetorno.cs
+++ Classes_Metodos/MetodosComRetorno.cs
@@ -22,15 +22,48 @@ namespace CursoCSharp.Classes_Metodos {
         public int Divisao(int num1, int num2) {
             return num1 / num2;
         }
+       
     }
+    // 
+   class CalculadoraCadeada {
+
+        int memoria;
+        public CalculadoraCadeada Somar(int num1) { 
+            memoria += num1;
+            return this;
+        }public CalculadoraCadeada Subtrair(int num1) { 
+            memoria -= num1;
+            return this;
+        }public CalculadoraCadeada Multiplicar(int num1) { 
+            memoria *= num1;
+            return this;
+        }public CalculadoraCadeada Dividir(int num1) { 
+            memoria /= num1;
+            return this;
+        }
+        public CalculadoraCadeada Limpar() {
+            memoria = 0;
+            return this;
+        }
+        public CalculadoraCadeada Imprimir() {
+            Console.WriteLine(memoria);
+            return this;
+        }
+        public int Resultado() {
+            return memoria;
+        }
+    }
+
     class MetodosComRetorno {
         public static void Executar() {
             var calculadoraComum = new CalculadoraComum();
-  
+
             Console.WriteLine($"{calculadoraComum.Somar(6, 2)} - {calculadoraComum.Subtrair(81, 2)} - " +
                 $"{calculadoraComum.Multiplicar(50, 2)} - {calculadoraComum.Divisao(154, 2)}");
 
-
+            // retonor em cadeamento
+            var calculadoraCadeada = new CalculadoraCadeada();
+            calculadoraCadeada.Somar(6).Subtrair(5).Multiplicar(6).Dividir(3).Limpar().Imprimir();
         }
     }
 }
