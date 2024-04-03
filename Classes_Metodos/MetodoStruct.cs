dd7d220192a20ca7d18afa0a882d3ef32a167998
isaias.costa <isaias.costa@ATA-TI20>
2024-04-03 08:22:24 -0300

    Commit Struct

--- Classes_Metodos/MetodoStruct.cs
+++ Classes_Metodos/MetodoStruct.cs
@@ -5,10 +5,35 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CursoCSharp.Classes_Metodos {
-    internal class MetodoStruct {
+
+    interface Ponto {
+        void MoverNaDiagonal(int delta);
+    }
+    struct Coodernada : Ponto {
+        public int X;
+        public int Y;
+
+        public Coodernada(int x, int y) {
+            X = x;
+            Y = y;
+        }
+        public void MoverNaDiagonal(int delta) {
+            X += delta;
+            Y += delta;
+        }
+    }
+    class MetodoStruct {
 
         public static void Executar() {
-
+            Coodernada coodenadaInicial;
+            coodenadaInicial.X = 2;
+            coodenadaInicial.Y = 2;
+            Console.WriteLine($"CoodernadaInicia do X = {coodenadaInicial.X}-CoodernadaInicia.Y = {coodenadaInicial.Y}");
+            var coodenadaFinal = new Coodernada(x:7, y:3);
+            coodenadaFinal.MoverNaDiagonal(15);
+            Console.WriteLine("Coodernada Final ");
+            Console.WriteLine($"X = {coodenadaFinal.X}");
+            Console.WriteLine($"Y = {coodenadaFinal.Y}");
 
         }
     }
