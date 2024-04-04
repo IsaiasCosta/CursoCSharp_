b38b5e68842443aae1a12b429049f7a60c526eeb
isaias.costa <isaias.costa@ATA-TI20>
2024-04-04 11:38:45 -0300

    Commit herança - usando polimorfismo

--- POO/Heranca.cs
+++ POO/Heranca.cs
@@ -19,8 +19,8 @@
             }
             return VelocidadeAtual;
         }
-        public int Acelerar() {
-            return AlterarVelocidade(5*5);
+        public virtual int Acelerar() { // Metodo com virtual pode serr sobre escrito, com a notação override
+            return AlterarVelocidade(5);
         }
         public int Freiar() {
             return AlterarVelocidade(-5);
@@ -30,9 +30,20 @@
 
     public class Uno : Carro {
         public Uno() : base(210) { }
-    } 
+    }
     public class Lamborguine : Carro {
-        public Lamborguine() : base(380) { }
+        public Lamborguine() : base(380) {
+
+        }
+        //Sobre escrever o metodo Acelerar, mas ou sobre escrever não esqueça de ir ao metodo
+        //e alterar o metodo da classe Pai com a notação virtual
+        public override int Acelerar() {
+            return AlterarVelocidade(50);
+        }
+        // notação new oculta da classe Pai
+        public new int Frear() {
+            return AlterarVelocidade(-5);
+        }
     }
     internal class Heranca {
 
@@ -51,12 +62,34 @@
             Console.WriteLine($"Acelerando {carro2.Acelerar()} ");
             Console.WriteLine($"Acelerando {carro2.Acelerar()} ");
             Console.WriteLine($"Acelerando {carro2.Acelerar()} ");
-            Console.WriteLine($"Freiando {carro1.Freiar()}"); 
+            Console.WriteLine($"Freiando {carro1.Freiar()}");
             Console.WriteLine($"Freiando {carro1.Freiar()}");
             Console.WriteLine($"Freiando {carro1.Freiar()}");
             Console.WriteLine($"Freiando {carro1.Freiar()}");
 
+            Console.WriteLine("Lamborguine com o tipo Carro");
 
+            Carro carro3 = new Lamborguine();
+            Console.WriteLine($"Acelerando {carro3.Acelerar()} ");
+            Console.WriteLine($"Acelerando {carro3.Acelerar()} ");
+            Console.WriteLine($"Acelerando {carro3.Acelerar()} ");
+            Console.WriteLine($"Acelerando {carro3.Acelerar()} ");
+            Console.WriteLine($"Freiando {carro3.Freiar()}");
+            Console.WriteLine($"Freiando {carro3.Freiar()}");
+            Console.WriteLine($"Freiando {carro3.Freiar()}");
+            Console.WriteLine($"Freiando {carro3.Freiar()}");
+
+            //Polimorfismo
+            Console.WriteLine("Uno com tipo carro");
+            carro3 = new Uno();
+            Console.WriteLine($"Acelerando {carro3.Acelerar()} ");
+            Console.WriteLine($"Acelerando {carro3.Acelerar()} ");
+            Console.WriteLine($"Acelerando {carro3.Acelerar()} ");
+            Console.WriteLine($"Acelerando {carro3.Acelerar()} ");
+            Console.WriteLine($"Freiando {carro3.Freiar()}");
+            Console.WriteLine($"Freiando {carro3.Freiar()}");
+            Console.WriteLine($"Freiando {carro3.Freiar()}");
+            Console.WriteLine($"Freiando {carro3.Freiar()}");
         }
     }
 }
